a573ead4e410092f7c6eb5f1691c7ae7da08985f
Dave New <dave@whereismytransport.com>
2021-09-15 09:26:34 +0200

    Using --logtostderr

--- src/pulumi-go.sh
+++ src/pulumi-go.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#set -e
-
 # Go to Pulumi project directory (mounted at runtime).
 cd project
 
@@ -21,3 +19,5 @@ pulumi login $PULUMI_BACKEND --non-interactive --color never --logtostderr
 # Requires ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID environment variables.
 # These provide Azure privileges by service principal - https://www.pulumi.com/docs/intro/cloud-providers/azure/setup/#service-principal-authentication
 pulumi $@ --non-interactive --color never --logtostderr
+
+# Note: pulumi does not use standard exit codes so using stderr is necessary
